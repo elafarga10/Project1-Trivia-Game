6d2fc6e947fd76b7f2ff74a8e52cc05d3b0490db
elafarga10 <elafarga10@gmail.com>
2020-04-08 22:37:24 -0700

    First commit

---
+++ script.js
@@ -0,0 +1,21 @@
+//In html we need to create 10 questions and their corresponding answers
+
+//the answers should be assigned to a button or some element user will click on
+
+//HTML should also include the Trivia Game title
+
+//In js we will need to target the buttons and add event listeners using event propagation
+
+//we will also need to target the questions by class
+
+//separate question by functions and use each to display the question and answers 
+
+//when user clicks on right question, button turns green and rest turn red
+
+//once user answers question, next question needs to be displayed along with its own corresponding answer choices
+
+//process repeats
+
+//once final question is answered, screen displays score 
+
+//In summary: app should be able to display question along with corresponding answers in buttons, when user clicks on right answer it will display as green or correct, if the user selects wrong answer it will display as wrong, then a new question will appear along with its own corresponding answers and the process repeats.
